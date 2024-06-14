da98b2c8d79a0debcd4aceb5b848930c2803da59
Joseph Morán <josephmoran@duck.com>
2024-06-13 21:51:09 -0500

    debbuging y comentarios

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+echo "primera linea de código php \n";
+
+$nombre = "Joseph";
+$apellido = "Morán";
+
+// echo "Yo me llamo" . " " . $nombre . " " . $apellido . "\n";
+
+// echo "Yo me llamo $nombre $apellido \n";
+
+// echo "El resultado de 4 x 5 es " . (4 * 5) .  "\n";
+
+$bases = [
+    "Adenina" => 1,
+    "Guanina" => 2,
+    "Citosina" => 3,
+    "Uracilo" => 4
+];
+
+print_r( $bases );
+
+echo "\n";
+
+//comentario
+# comentario
+
+/*
+aqui
+se
+puede 
+comentar 
+mucho
+más
+*/
+
